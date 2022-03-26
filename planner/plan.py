48cc95da19eabaca5b80ee0f3555fa33d77c1dce
Nikhil Abraham Mathew <nikhilmath99@gmail.com>
2022-03-27 02:15:30 +0530

    Adds login page and base html file

---
+++ planner/plan.py
@@ -0,0 +1,11 @@
+from . import db
+import datetime
+from flask import Flask, g, flash, Blueprint, render_template, request, redirect, url_for
+
+bp = Blueprint('plan', 'plan', url_prefix = '')
+
+@bp.route('/')
+def dashboard():
+  return render_template('login.html')
+  
+
